fab7c1bedc936c43817e8f4b0a27b7161788773e
14453 <1445372429@qq.com>
2025-05-27 23:31:18 +0800

    Initial commit: 新闻热点界面布局

--- app/src/main/java/qz/rg/newspaper/fragment/HotFragment.java
+++ app/src/main/java/qz/rg/newspaper/fragment/HotFragment.java
@@ -42,7 +42,7 @@ public class HotFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_video, container, false);
+        return inflater.inflate(R.layout.fragment_hot, container, false);
     }
 
     @Override
